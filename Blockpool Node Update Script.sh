83a1f0b1f160ab66e9e150263ceb81a9ac0c9a3d
locohammerhead <locohammerhead@gmail.com>
2017-10-10 16:29:32 -0400

    Add files via upload
    Updates node software

---
+++ Blockpool Node Update Script.sh
@@ -0,0 +1,6 @@
+forever stop app.js -c config.mainnet.json -g genesisBlock.mainnet.json
+cd BPL-node
+cp config.mainnet.json config1.mainnet.json
+git pull 
+cp config1.mainnet.json config.mainnet.json
+forever start app.js -c config.mainnet.json -g genesisBlock.mainnet.json
