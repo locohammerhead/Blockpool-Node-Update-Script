8bd9cb43647b1b7003428e2fe41a14f2798f3159
locohammerhead <locohammerhead@gmail.com>
2017-10-12 11:04:39 -0400

    Update Blockpool_Node_Update_Script.sh

--- Blockpool_Node_Update_Script.sh
+++ Blockpool_Node_Update_Script.sh
@@ -3,13 +3,13 @@ pkill -9 node
 forever stop app.js -c config.mainnet.json -g genesisBlock.mainnet.json
 cd BPL-node
 #create backup of the config file
-mv config.mainnet.json config.mainnet.backup.json
+cp config.mainnet.json config.mainnet.backup.json
 #checkout the config file
 git checkout config.mainnet.json
 #pull fresh code
 git pull origin mainnet
-mv config.mainnet.backup.json config.mainnet.json
+cp config.mainnet.backup.json config.mainnet.json
 #install forever dependency
 npm install forever -g
 #run node
-
+forever start app.js -c config.mainnet.json -g genesisBlock.mainnet.json
